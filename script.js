9d20296dba9c916f846336a76361a69d39acec54
Jarax <85376401+Jarax73@users.noreply.github.com>
2022-05-11 22:05:01 +0000

    page stylisé

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 const divContacts = document.createElement("div");
-const contactsName = document.createElement("h1");
-const contactGroup = document.createElement("h2");
-const contactBio = document.createElement("p");
+const contactPhoto = document.createElement("img");
+const toRemove = document.querySelector("#toRemove");
 
-const mainSection = document.querySelector("section");
+const mainSection = document.querySelector("#contact-list");
 const emptyParagraph = document.querySelector("#temporaire");
-
 const form = document.querySelector("form");
 
 form.addEventListener("submit", function(event){
 	event.preventDefault();
 	const contactItems = {};
-	for (let detail of event.target){
-		console.log(detail);
-		const {name, value} = detail;
-	 	if(detail){
-			contactItems[name] = value;
-			let array = contactItems;
-		}
-		
-	}
-	document.write(array);
+	const prenom = event.target.firstName.value;
+	const nom = event.target.lastName.value;
+	const groupe = event.target.group.value;
+	const info = event.target.bio.value;
+	const file = event.target.photo.value;
 	
+	for (let detail of event.target){
+		if(detail == undefined){
+			alert("Remplissez tous les champs");
+		}else{
+			
+			toRemove.remove();
+			
+		}
+			
+	}	
+	divContacts.innerHTML += `<p><img src="${file}" alt="photo de ${prenom}"/> ${prenom} ${nom} <br/>${groupe}</p><p>${info}</p>`;
+	mainSection.appendChild(divContacts);
+	mainSection.style = "display: default";
 })
+
+// const div1 = document.createElement("div");
+// const listContact = document.querySelector("#contact-list");
+
+// div1.innerHTML = `<p>Comment ça va</p><p>Je voudrais tester comment ça marche et voir comment l'intégrer</p>`;
+// if(div1){
+// 	listContact.firstChild.remove();
+// 	listContact.appendChild(div1);
+// }
+
