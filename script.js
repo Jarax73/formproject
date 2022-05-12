da88fb94d35d3fb06bd9a863cd664eb97c73c342
Jarax <85376401+Jarax73@users.noreply.github.com>
2022-05-12 22:21:12 +0000

    afficher les valeurs recueillies du formulaire

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
 const divContacts = document.createElement("div");
-const contactPhoto = document.createElement("img");
-const toRemove = document.querySelector("#toRemove");
-
+divContacts.setAttribute("id", "styliser");
 const mainSection = document.querySelector("#contact-list");
-const emptyParagraph = document.querySelector("#temporaire");
 const form = document.querySelector("form");
+const paragraph = document.createElement('p');
 
 form.addEventListener("submit", function(event){
 	event.preventDefault();
-	const contactItems = {};
+	
 	const prenom = event.target.firstName.value;
 	const nom = event.target.lastName.value;
 	const groupe = event.target.group.value;
 	const info = event.target.bio.value;
 	const file = event.target.photo.value;
 	
-	for (let detail of event.target){
-		if(detail == undefined){
-			alert("Remplissez tous les champs");
-		}else{
-			
-			toRemove.remove();
-			
-		}
-			
-	}	
-	divContacts.innerHTML += `<p><img src="${file}" alt="photo de ${prenom}"/> ${prenom} ${nom} <br/>${groupe}</p><p>${info}</p>`;
+	divContacts.innerHTML += `<div class="bloc-contact">
+															<p class="photo"><img  alt="Photo de ${prenom}"/></p>
+															<div class="info">
+																<p class="info-list"> ${prenom} ${nom} <br/>${groupe}</p>
+																<p class="info-list">${info}</p>
+															</div>
+															<p class="fermeture"><img src="croix.png" /><p>
+														</div>`;
+
+	
+	
+	
+
 	mainSection.appendChild(divContacts);
-	mainSection.style = "display: default";
-})
+	const infoList = document.getElementsByClassName("info-list");
+	mainSection.style.display = "block";
+	let fermeture = document.querySelector("#fermeture img")
+	fermeture.style.width = "20px";
+});
 
-// const div1 = document.createElement("div");
-// const listContact = document.querySelector("#contact-list");
+// 	const openFile = function(event) {
+//   const input = event.target;
 
-// div1.innerHTML = `<p>Comment ça va</p><p>Je voudrais tester comment ça marche et voir comment l'intégrer</p>`;
-// if(div1){
-// 	listContact.firstChild.remove();
-// 	listContact.appendChild(div1);
+//   const reader = new FileReader();
+//   reader.onload = function(){
+//     const dataURL = reader.result;
+//     const output = document.querySelector('.photo img');
+//     output.src = dataURL;
+//   };
+//   reader.readAsDataURL(input.files[0]);
 // }
 
+// const values = {};
+// 	for (const field of this.elements){
+// 		const{name, value} = field;
+// 		if(name){
+// 			values[name] = value;
+// 		}
+// 	}
+// 	const div1 = document.createElement('div');
+// 	div1.setAttribute('class', 'bloc-contact');
+	
+// 	paragraph += values.name;
+// 	div1.appendChild(paragraph);
+// 	divContacts.appendChild(div1);
