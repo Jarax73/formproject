383a48f55a84ef02b5c5b4626e68952f509cce89
Jarax <85376401+Jarax73@users.noreply.github.com>
2022-05-16 14:48:33 +0000

    modification et suppression de contacts

--- script.js
+++ script.js
@@ -2,101 +2,17 @@ let divContacts = document.createElement("div");
 divContacts.setAttribute("class", "styliser");
 const mainSection = document.querySelector("#contact-list");
 mainSection.appendChild(divContacts);
-const form = document.querySelector("form");
+const form = document.querySelector("form"); 
+
+const paragraph = document.createElement('p');
 const divUnique = document.createElement('div');
-divUnique.setAttribute('id', 'bloc-contact');
-const eventModifier = document.createElement('div');
-eventModifier.setAttribute('id', 'event');
 
-const photoContact = document.createElement("p");
-photoContact.setAttribute("class", "photo");
-const imageContact = document.createElement("img");
-
-photoContact.appendChild(imageContact);
-const divInfo = document.createElement("div");
-divInfo.setAttribute("class", "info");
-const nomsContact = document.createElement('h2');
-const groupContact = document.createElement('h3');
-const infoContact = document.createElement('p');
-divInfo.appendChild(nomsContact);
-divInfo.appendChild(groupContact);
-divInfo.appendChild(infoContact);
-const closeP = document.createElement('p');
-const link = document.createElement('a')
-link.setAttribute("class", "fermeture");
-const imageClose = document.createElement('img');
-imageClose.setAttribute("src", "croix.png");
-link.appendChild(imageClose);
-closeP.appendChild(link);
-divContacts.appendChild(closeP);
-eventModifier.appendChild(photoContact);
-eventModifier.appendChild(divInfo);
-divUnique.appendChild(eventModifier);
-divUnique.appendChild(closeP);
-divContacts.appendChild(divUnique);
-
+divUnique.setAttribute("id", "bloc-contact");
 
 form.addEventListener("submit", function(event){
 	event.preventDefault();
 
-	
-	const prenom = event.target.firstName.value;
-	const nom = event.target.lastName.value;
-	const groupe = event.target.group.value;
-	const info = event.target.bio.value;
-	const file = event.target.photo.files[0];
-	const name = URL.createObjectURL(file);
-
-	imageContact.setAttribute("src", name);
-	imageContact.setAttribute("alt", `Photo de ${prenom}`);
-
-	nomsContact.textContent = `${prenom} ${nom}`;
-	groupContact.textContent = groupe;
-	infoContact.textContent = info;
-	// const uniq = 'id' + (new Date()).getTime();
-	//  divContacts.innerHTML += `<div id="bloc-contact">
-	// 														<div id="event" >
-	//  														<p class="photo"><img src="${name}" alt="Photo de ${prenom}"/></p>
-	//  														<div class="info">
-	//  															<h2> ${prenom} ${nom} </h2>
-	// 																<h3>${groupe}</h3>
-	//  															<p class="info-list">${info}</p>
-	//  														</div>
-	// 														</div>
-	//  														<p><a id="fermeture"><img src="croix.png"></a></p>
-	//  													</div>`;
-
-	// const blocContact = document.querySelector("#bloc-contact");
-	console.log(divContacts);
-
-	
-	// const divEvent = document.querySelector(`#event`);
-	eventModifier.addEventListener('click', () => {
-		console.log("ça marche");
-		// divUnique.remove();
-		event.target.firstName.value = prenom;
-		event.target.lastName.value	= nom  ;
-		event.target.group.value = groupe;
-		event.target.bio.value = info ;
-		//  = file ;
-		event.target.photo.files[0] = name ;
-			
-	})
-	// const closeP = document.querySelector(`#fermeture`);
-	closeP.addEventListener("click", function() {
-	blocContact.remove()
-	});
-
-	mainSection.style.display = "block";
-	form.reset();
-});
-
-/* const paragraph = document.createElement('p');
-const divUnique = document.createElement('div');
-
-divUnique.setAttribute("id", "bloc-contact");*/
-
-// const inputValues = {};
+	// const inputValues = {};
 
 	// for(const champ of this.elements){
 	// 	const{name, value} = champ;
@@ -106,36 +22,85 @@ divUnique.setAttribute("id", "bloc-contact");*/
 			
 	// 	}
 	// }
+	const prenom = event.target.firstName.value;
+	const nom = event.target.lastName.value;
+	const groupe = event.target.group.value;
+	const info = event.target.bio.value;
+	const file = event.target.photo.files[0];
+	const name = URL.createObjectURL(file);
 
-/*	const photoContact = document.createElement("p");
+	const photoContact = document.createElement("p");
 	photoContact.setAttribute("class", "photo");
 	const imageContact = document.createElement("img");
 	imageContact.setAttribute("src", name);
 	imageContact.setAttribute("alt", `Photo de ${prenom}`);
-	photoContact.appendChild(imageContact);
+	photoContact.append(imageContact);
 	const divInfo = document.createElement("div");
 	divInfo.setAttribute("class", "info");
 	const nomsContact = document.createElement('h2');
 	const groupContact = document.createElement('h3');
 	const infoContact = document.createElement('p');
-	divInfo.appendChild(nomsContact);
-	divInfo.appendChild(groupContact);
-	divInfo.appendChild(infoContact);
+	const divEvent = document.createElement('div');
+	divEvent.setAttribute('class', 'event');
+	
+	divInfo.append(nomsContact);
+	divInfo.append(groupContact);
+	divInfo.append(infoContact);
+	divEvent.append(photoContact);
+	divEvent.append(photoContact);
+	divEvent.append(divInfo);
 	 const closeP = document.createElement('p');
 	const link = document.createElement('a')
 	link.setAttribute("id", "fermeture");
 	const imageClose = document.createElement('img');
 	imageClose.setAttribute("src", "croix.png");
-	link.appendChild(imageClose);
-	closeP.appendChild(link);
-divContacts.appendChild(closeP);
-	/*divUnique.appendChild(photoContact);
-	divUnique.appendChild(divInfo);
-	divUnique.appendChild(closeP);
-	divContacts.appendChild(divUnique);
-	*/
+	link.append(imageClose);
+	closeP.append(link);
+	
+	divEvent.append(closeP);
+	divUnique.append(divEvent);
+	
+	divContacts.append(divUnique);
+	
+	
+	 // divContacts.innerHTML += `<div class="bloc-contact">
+		// 													<div class="event">
+	 // 														<p class="photo"><img src="${name}" alt="Photo de ${prenom}"/></p>
+	 // 														<div class="info">
+	 // 															<h2> ${prenom} ${nom} </h2>
+		// 														<h3>${groupe}</h3>
+	 // 															<p class="info-list">${info}</p>
+	 // 														</div>
+		// 													</div>
+	 // 														<p><a class="fermeture"><img src="croix.png"></a></p>
+	 // 													</div>`;
 
-/*nomsContact.textContent = `${prenom} ${nom}`;
+	// const blocContact = document.querySelector(".bloc-contact");
+	console.log(mainSection);
+	nomsContact.textContent = `${prenom} ${nom}`;
 	groupContact.textContent = groupe;
 	infoContact.textContent = info;
-	*/
+	
+	
+	// const divEvent = document.querySelector(".event");
+	divInfo.addEventListener('click', () => {
+		console.log("ça marche");
+		event.target.firstName.value = prenom;
+		event.target.lastName.value	= nom  ;
+		event.target.group.value = groupe;
+		event.target.bio.value = info ;
+		event.target.photo.files[0] = name ;
+		
+	})
+	// const closeP = document.querySelector(".fermeture");
+closeP.addEventListener("click", () => divEvent.remove());
+
+	// blocContact.addEventListener("click", function(){
+	// 	blocContact.remove();
+	// })
+	
+	
+	const infoList = document.getElementsByClassName("info-list");
+	mainSection.style.display = "block";
+	form.reset();
+});
