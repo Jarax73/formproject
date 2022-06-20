302f1042c00d5163934b2fcf39fa4e5e3fb6de3e
Jared-Alexandre Kapila <85376401+Jarax73@users.noreply.github.com>
2022-06-20 09:50:30 +0000

    review : modify content

--- script.js
+++ script.js
@@ -3,31 +3,19 @@ divContacts.setAttribute("class", "bloc-contact");
 const mainSection = document.querySelector("#contact-list");
 mainSection.appendChild(divContacts);
 const form = document.querySelector("form"); 
-
 const paragraph = document.createElement('p');
-const divUnique = document.createElement('div');
+const submitButton = document.querySelector("[type='submit']");
 
-// divUnique.setAttribute("id", "bloc-contact");
 
 form.addEventListener("submit", function(event){
 	event.preventDefault();
 
-	// const inputValues = {};
-
-	// for(const champ of this.elements){
-	// 	const{name, value} = champ;
-	// 	if(name){
-	// 		inputValues[name] = value;
-	// 		alert(inputValues[name]);
-			
-	// 	}
-	// }
-	const prenom = event.target.firstName.value;
-	const nom = event.target.lastName.value;
-	const groupe = event.target.group.value;
-	const info = event.target.bio.value;
-	const file = event.target.photo.files[0];
-	const name = URL.createObjectURL(file);
+	let prenom = event.target.firstName.value;
+	let nom = event.target.lastName.value;
+	let groupe = event.target.group.value;
+	let info = event.target.bio.value;
+	let file = event.target.photo.files[0];
+	let name = URL.createObjectURL(file);
 
 	const photoContact = document.createElement("p");
 	photoContact.setAttribute("class", "photo");
@@ -41,46 +29,52 @@ form.addEventListener("submit", function(event){
 	const groupContact = document.createElement('h3');
 	const infoContact = document.createElement('p');
 	const divEvent = document.createElement('div');
-	divEvent.setAttribute('class', 'event');
 	
+	divEvent.setAttribute('class', 'event');
 	divInfo.append(nomsContact);
 	divInfo.append(groupContact);
 	divInfo.append(infoContact);
 	divEvent.append(photoContact);
 	divEvent.append(photoContact);
 	divEvent.append(divInfo);
-	 const closeP = document.createElement('p');
+	
+	const closeP = document.createElement('p');
 	const link = document.createElement('a')
 	link.setAttribute("id", "fermeture");
 	const imageClose = document.createElement('img');
 	imageClose.setAttribute("src", "croix.png");
 	link.append(imageClose);
 	closeP.append(link);
-	
 	divEvent.append(closeP);
 	divContacts.append(divEvent);
-	
-	console.log(mainSection);
 	nomsContact.textContent = `${prenom} ${nom}`;
 	groupContact.textContent = groupe;
 	infoContact.textContent = info;
 	
-	divInfo.addEventListener('click', () => {
+	divEvent.addEventListener('click', () => {
 		console.log("ça marche");
+		submitButton.value = "Modifier";
 		event.target.firstName.value = prenom;
-		event.target.lastName.value	= nom  ;
+		event.target.lastName.value	= nom;
 		event.target.group.value = groupe;
-		event.target.bio.value = info ;
-		event.target.photo.files[0] = name ;
-		
+		event.target.bio.value = info;
+		event.target.photo.files[0] = name;
+		form.addEventListener("submit", function (e) {
+			e.preventDefault();
+			divEvent.innerHTML = prenom;
+			divEvent.innerHTML = nom;
+			divEvent.innerHTML = groupe;
+			divEvent.innerHTML = info;
+			divEvent.innerHTML = file;
+			divEvent.innerHTML = name;
+		})
 	})
-closeP.addEventListener("click", () => divEvent.remove());
-const eventFile = document.querySelector("#event-file");
+	closeP.addEventListener("click", () => divEvent.remove());
+	const eventFile = document.querySelector("#event-file");
 	eventFile.addEventListener('click', function(){
 		const imageSource = document.querySelector("#photo");
 		imageSource.setAttribute("src", name);
 	})
-	
 	
 	const infoList = document.getElementsByClassName("info-list");
 	mainSection.style.display = "block";
