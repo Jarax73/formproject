1bf5cf98967a669f2644118400dd4027c1af3829
Jarax <jaraxkapila73@gmail.com>
2022-12-22 19:45:45 +0100

    Feature: get one user from database

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -16,6 +16,12 @@ exports.createContact = (request, response) => {
 
 exports.getContacts = (request, response) => {
     Contact.find()
-    .then(contacts => response.status(200).json({ contacts }))
-    .catch((error) => response.status(400).json({ error }));
+        .then(contacts => response.status(200).json({ contacts }))
+        .catch((error) => response.status(500).json({ error }));
 }
+
+exports.getContact = (request, response) => {
+    Contact.find({_id: request.params.id})
+        .then(contact => response.status(200).json({ contact }))
+        .catch((error) => response.status(500).json({ error }))
+};
--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.post("/", contactController.createContact);
 router.get("/", contactController.getContacts);
+router.get("/:id", contactController.getContact)
 
 module.exports = router;
